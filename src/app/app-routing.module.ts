0dd793d0a8a6c48ee79c00abab9653c7519b3997
FacundoFalcioni2410 <facundofalcioni2410@gmail.com>
2021-08-27 08:59:08 -0300

    deploy heroku

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { QuienSoyComponent} from './components/quien-soy/quien-soy.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'about', component: QuienSoyComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
