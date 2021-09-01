44edbdf249c81419453db1a2d7c79fc0a421e3fa
FacundoFalcioni2410 <facundofalcioni2410@gmail.com>
2021-08-31 23:28:38 -0300

    sprint 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyB6Hlfh5Jcs_mGo39zqVXs0tpt1mtHNTrs",
+    authDomain: "sala-de-juegos-e5892.firebaseapp.com",
+    projectId: "sala-de-juegos-e5892",
+    storageBucket: "sala-de-juegos-e5892.appspot.com",
+    messagingSenderId: "357950147430",
+    appId: "1:357950147430:web:2b3a0e1f12fccd543f56a7"
+  }
 };
